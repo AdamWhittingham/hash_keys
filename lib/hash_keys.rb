2d1ad53d493f57fa8584f4eb1410111853aadf47
Adam Whittingham <adam.whittingham@gmail.com>
2014-06-25 23:07:47 +0100

    Add some comments/rdoc to the new hash methods

--- lib/hash_keys.rb
+++ lib/hash_keys.rb
@@ -1,10 +1,12 @@
 class Hash
+  # Return a hash containing only the named keys
   def select_keys *keys
     select{|k, _| keys.include? k}
   end
 
   alias_method :retain_keys, :select_keys
 
+  # Return a hash containing everything except the named keys
   def reject_keys *keys
     reject{|k, _| keys.include? k}
   end
