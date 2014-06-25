bdd67467c34dd5a989be13c8951a920f0871bd7d
Adam Whittingham <adam.whittingham@gmail.com>
2014-06-25 23:10:18 +0100

    Rename spec file to mirror prod code

--- spec/hash_keys_spec.rb
+++ spec/hash_keys_spec.rb
@@ -4,13 +4,13 @@ describe Hash do
   subject { {foo: 1, bar: 2, baz: 3} }
 
   describe '#select_keys' do
-    it 'selects only the listed keys' do
+    it 'selects only the named keys' do
       expect(subject.select_keys(:foo, :baz)).to eq({foo: 1, baz: 3})
     end
   end
 
   describe '#reject_keys' do
-    it 'rejects the listed keys' do
+    it 'returns any keys not named' do
       expect(subject.reject_keys(:foo)).to eq({bar: 2, baz: 3})
     end
   end
